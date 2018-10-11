352f69ccd2cd23cfe4376eb4d21f2eba80528f15
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-11 08:31:17 -0600

    locales added

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './plugins/vue-i18n';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import i18n from './i18n';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -14,5 +15,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
