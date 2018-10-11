ff7b639d19c6adb42c1952272c4e1aee80a6c51f
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-11 08:30:22 -0600

    vue plugins required on app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import '@babel/polyfill'
 import Vue from "vue";
+import './plugins/axios';
+import './plugins/vuetify';
+import './plugins/vue-i18n';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false;
 
