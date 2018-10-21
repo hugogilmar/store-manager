26859a8aed3f764d7b95afdee54f725223a0a87b
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-20 19:20:19 -0600

    semicolons added

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import i18n from './i18n';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
 
