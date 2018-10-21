f68d0c0a7b3580ac1327ad5f5c84ef2a47fe1877
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-21 14:26:40 -0600

    fix indentation for dialog component

--- src/modules/product_categories/components/ProductCategoryDeleteDialog.vue
+++ src/modules/product_categories/components/ProductCategoryDeleteDialog.vue
@@ -1,48 +1,48 @@
 <template>
   <v-dialog
-      v-model="dialog"
-      width="500"
+    v-model="dialog"
+    width="500"
+  >
+    <v-btn
+      color="error"
+      slot="activator"
+      dark
+      fab
+      bottom
+      right
+      fixed
     >
-      <v-btn
-        color="error"
-        slot="activator"
-        dark
-        fab
-        bottom
-        right
-        fixed
+      <v-icon>delete</v-icon>
+    </v-btn>
+    <v-card>
+      <v-card-title
+        class="headline grey lighten-2"
+        primary-title
       >
-        <v-icon>delete</v-icon>
-      </v-btn>
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
+        {{ $t('dialog.delete.title', { entity: $tc('entities.productCategory', 1) }) }}
+      </v-card-title>
+      <v-card-text>
+        {{ $t('dialog.delete.message', { entity: $tc('entities.productCategory', 1) }) }}
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          flat
+          @click="dialog = false"
         >
-          {{ $t('dialog.delete.title', { entity: $tc('entities.productCategory', 1) }) }}
-        </v-card-title>
-        <v-card-text>
-          {{ $t('dialog.delete.message', { entity: $tc('entities.productCategory', 1) }) }}
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            flat
-            @click="dialog = false"
-          >
-            {{ $t('label.cancel') }}
-          </v-btn>
-          <v-btn
-            color="primary"
-            flat
-            @click="deleteProductCategory"
-          >
-            {{ $t('label.confirm') }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+          {{ $t('label.cancel') }}
+        </v-btn>
+        <v-btn
+          color="primary"
+          flat
+          @click="deleteProductCategory"
+        >
+          {{ $t('label.confirm') }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
