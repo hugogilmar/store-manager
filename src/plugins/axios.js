79a5b9e18b92e5ea561e8db80df63be443bbf4bb
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-28 19:05:49 -0600

    single quotes for imports

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import axios from "axios";
+import axios from 'axios';
 import NProgress from 'nprogress';
 
 // Full config:  https://github.com/axios/axios#request-config
--- src/router.js
+++ src/router.js
@@ -1,20 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 import NProgress from 'nprogress';
-import Home from "./views/Home.vue";
-import productsRoutes from "./modules/products/routes";
-import productCategoriesRoutes from "./modules/product_categories/routes";
-import storesRoutes from "./modules/stores/routes";
-import paymentMethodsRoutes from "./modules/payment_methods/routes";
-import employeesRoutes from "./modules/employees/routes";
-import ordersRoutes from "./modules/orders/routes";
+import Home from './views/Home.vue';
+import productsRoutes from './modules/products/routes';
+import productCategoriesRoutes from './modules/product_categories/routes';
+import storesRoutes from './modules/stores/routes';
+import paymentMethodsRoutes from './modules/payment_methods/routes';
+import employeesRoutes from './modules/employees/routes';
+import ordersRoutes from './modules/orders/routes';
+import sessionsRoutes from './modules/sessions/routes';
 
 Vue.use(Router);
 
 const appRoutes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: Home
   }
 ];
@@ -29,7 +30,7 @@ const routes = appRoutes.concat(
 );
 
 const router = new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: routes
 });
