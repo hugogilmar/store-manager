53a8a4887afc9219dcae3121ede0c28ee51ce67b
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-10-28 20:56:38 -0600

    include authentication token header on requests

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,41 +1,44 @@
 import Vue from 'vue';
 import axios from 'axios';
+import store from '../store';
 import NProgress from 'nprogress';
-
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
   baseURL: process.env.VUE_APP_API_BASE
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
   function(config) {
-    // Do something before request is sent
+    if (store.getters.authenticationToken) {
+      config.headers.Authorization = store.getters.authenticationToken;
+    }
+
     NProgress.start();
+
     return config;
   },
   function(error) {
-    // Do something with request error
+    NProgress.done();
+
     return Promise.reject(error);
   }
 );
 
-// Add a response interceptor
 _axios.interceptors.response.use(
   function(response) {
-    // Do something with response data
     NProgress.done();
+
     return response;
   },
   function(error) {
-    // Do something with response error
+    NProgress.done();
+
+    if (error.response.status === 401) {
+      store.dispatch('logout');
+    }
+
     return Promise.reject(error);
   }
 );
