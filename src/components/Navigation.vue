b1c340c619eabb868be352e3004d11fb2c325312
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-03 15:18:51 -0600

    navigation groups added

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,25 +3,39 @@
     persistent
     permanent
     fixed
+    dark
     app
   >
     <v-list>
-      <v-list-tile
-        value="true"
-        v-for="(item, i) in items"
+      <v-subheader>
+        {{ $t('navigation.title') }}
+      </v-subheader>
+      <v-list-group
+        prepend-icon="menu"
+        v-for="(group, i) in groups"
         :key="i"
-        @click="navigateTo(item)"
       >
-        <v-list-tile-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-      <v-divider></v-divider>
+        <v-list-tile slot="activator">
+          <v-list-tile-title>
+            {{ group.title }}
+          </v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile
+          v-for="(item, j) in items(group.group)"
+          :key="j"
+          @click="navigateTo(item)"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.title }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list-group>
       <v-list-tile
-        value="true"
         @click.stop="logout"
       >
         <v-list-tile-action>
@@ -43,41 +57,72 @@
     data () {
       return {
         clipped: false,
-        items: [
+        groups: [
           {
-            icon: 'shopping_cart',
-            title: this.$t('navigation.products'),
-            path: '/products'
-          },
-          {
-            icon: 'bookmark',
-            title: this.$t('navigation.productCategories'),
-            path: '/product_categories'
-          },
-          {
-            icon: 'store',
-            title: this.$t('navigation.stores'),
-            path: '/stores'
-          },
-          {
-            icon: 'attach_money',
-            title: this.$t('navigation.paymentMethods'),
-            path: '/payment_methods'
-          },
-          {
-            icon: 'person',
-            title: this.$t('navigation.employees'),
-            path: '/employees'
-          },
-          {
-            icon: 'list',
-            title: this.$t('navigation.orders'),
-            path: '/orders'
+            icon: 'menu',
+            title: this.$t('navigation.groups.cruds'),
+            group: 'cruds'
           },
           {
             icon: 'show_chart',
-            title: this.$t('navigation.reports'),
-            path: '/reports'
+            title: this.$t('navigation.groups.reports'),
+            group: 'reports'
+          }
+        ],
+        links: [
+          {
+            icon: 'shopping_cart',
+            title: this.$t('navigation.links.products'),
+            path: '/products',
+            group: 'cruds'
+          },
+          {
+            icon: 'bookmark',
+            title: this.$t('navigation.links.productCategories'),
+            path: '/product_categories',
+            group: 'cruds'
+          },
+          {
+            icon: 'store',
+            title: this.$t('navigation.links.stores'),
+            path: '/stores',
+            group: 'cruds'
+          },
+          {
+            icon: 'attach_money',
+            title: this.$t('navigation.links.paymentMethods'),
+            path: '/payment_methods',
+            group: 'cruds'
+          },
+          {
+            icon: 'person',
+            title: this.$t('navigation.links.employees'),
+            path: '/employees',
+            group: 'cruds'
+          },
+          {
+            icon: 'list',
+            title: this.$t('navigation.links.orders'),
+            path: '/orders',
+            group: 'cruds'
+          },
+          {
+            icon: 'shopping_cart',
+            title: this.$t('navigation.links.productsReport'),
+            path: '/reports/product',
+            group: 'reports'
+          },
+          {
+            icon: 'bookmark',
+            title: this.$t('navigation.links.productCategoriesReport'),
+            path: '/reports/productCategory',
+            group: 'reports'
+          },
+          {
+            icon: 'attach_money',
+            title: this.$t('navigation.links.paymentMethodsReport'),
+            path: '/reports/paymentMethod',
+            group: 'reports'
           }
         ]
       }
@@ -86,9 +131,20 @@
       ...mapActions([
         'logout'
       ]),
+      items (group) {
+        return this.links.filter(function (link) {
+          return link.group == group
+        });
+      },
       navigateTo: function (item) {
         this.$router.push({ path: item.path });
       }
     }
   }
 </script>
+
+<style scoped>
+  .v-list__group__header--active .v-list__group__header__prepend-icon .v-icon {
+    color: #fff;
+  }
+</style>
