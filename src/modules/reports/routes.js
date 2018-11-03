b3dd35896a565d2b53fef92a13a8bc4951a64fd5
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-03 15:23:21 -0600

    products, categories and payment methods reports added

--- src/modules/reports/routes.js
+++ src/modules/reports/routes.js
@@ -1,8 +1,20 @@
 export default [
   {
-    path: "/reports",
-    name: "reports",
+    path: "/reports/product",
+    name: "product_report",
     component: () =>
-      import(/* webpackChunkName: "reports" */ "./views/List.vue")
+      import(/* webpackChunkName: "reports" */ "./views/Product.vue")
+  },
+  {
+    path: "/reports/productCategory",
+    name: "productCategory_report",
+    component: () =>
+      import(/* webpackChunkName: "reports" */ "./views/ProductCategory.vue")
+  },
+  {
+    path: "/reports/paymentMethod",
+    name: "paymentMethod_report",
+    component: () =>
+      import(/* webpackChunkName: "reports" */ "./views/PaymentMethod.vue")
   }
 ];
