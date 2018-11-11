c67aeab76c7bb677fc001b03d1a950abdba41d7d
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-11 14:08:07 -0600

    store filter added

--- src/modules/charges/components/ChargeList.vue
+++ src/modules/charges/components/ChargeList.vue
@@ -1,29 +1,45 @@
 <template>
-  <v-list>
-    <v-alert
-      :value="true"
-      type="info"
-      v-if="charges.length == 0"
-    >
-      {{ $t('alert.empty') }}
-    </v-alert>
-    <v-list-tile
-      v-for="charge in charges"
-      :key="charge.id"
-      avatar
-      @click="editCharge(charge.id)"
-    >
-      <v-list-tile-action>
-        <v-icon>star</v-icon>
-      </v-list-tile-action>
-      <v-list-tile-content>
-        <v-list-tile-title v-text="charge.name"></v-list-tile-title>
-      </v-list-tile-content>
-      <v-list-tile-avatar v-if="charge.avatar">
-        <img :src="charge.avatar">
-      </v-list-tile-avatar>
-    </v-list-tile>
-  </v-list>
+  <v-layout row wrap>
+    <v-flex xs3 class="pa-2">
+      <v-select
+        v-model="storeId"
+        :items="stores"
+        :rules="[v => !!v || 'Item is required']"
+        :label="$t('product.store')"
+        item-text="name"
+        item-value="id"
+        required
+      ></v-select>
+    </v-flex>
+    <v-flex xs9 class="pa-2">
+      <v-list two-line>
+        <v-alert
+          :value="true"
+          type="info"
+          v-if="charges.length == 0"
+        >
+          {{ $t('alert.empty') }}
+        </v-alert>
+        <v-list-tile
+          v-for="charge in charges"
+          :key="charge.id"
+          avatar
+          @click="editCharge(charge.id)"
+        >
+          <v-list-tile-action>
+            <v-icon>star</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ charge.name }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ charge.code }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-avatar v-if="charge.avatar">
+            <img :src="charge.avatar">
+          </v-list-tile-avatar>
+        </v-list-tile>
+      </v-list>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -31,17 +47,39 @@
     name: 'ChargeList',
     data () {
       return {
-        charges: []
+        storeId: null,
+        stores: [],
+        charges: [],
+        params: new URLSearchParams()
+      }
+    },
+    watch: {
+      storeId (value) {
+        this.params.set('filter[where][storeId]', value);
+        this.getCharges();
       }
     },
     created () {
-      this.getCharges();
+      this.getStores();
     },
     methods: {
+      getStores () {
+        let self = this;
+
+        this.$axios.get('/stores')
+        .then(function (response) {
+          self.stores = response.data;
+        })
+        .catch(function (error) {
+          self.stores = [];
+        });
+      },
       getCharges () {
         let self = this;
 
-        this.$axios.get('/charges')
+        this.$axios.get('/charges', {
+          params: this.params
+        })
         .then(function (response) {
           self.charges = response.data;
         })
--- src/modules/employees/components/EmployeeList.vue
+++ src/modules/employees/components/EmployeeList.vue
@@ -1,29 +1,45 @@
 <template>
-  <v-list>
-    <v-alert
-      :value="true"
-      type="info"
-      v-if="employees.length == 0"
-    >
-      {{ $t('alert.empty') }}
-    </v-alert>
-    <v-list-tile
-      v-for="employee in employees"
-      :key="employee.id"
-      avatar
-      @click="editEmployee(employee.id)"
-    >
-      <v-list-tile-action>
-        <v-icon>star</v-icon>
-      </v-list-tile-action>
-      <v-list-tile-content>
-        <v-list-tile-title v-text="employee.name"></v-list-tile-title>
-      </v-list-tile-content>
-      <v-list-tile-avatar v-if="employee.avatar">
-        <img :src="employee.avatar">
-      </v-list-tile-avatar>
-    </v-list-tile>
-  </v-list>
+  <v-layout row wrap>
+    <v-flex xs3 class="pa-2">
+      <v-select
+        v-model="storeId"
+        :items="stores"
+        :rules="[v => !!v || 'Item is required']"
+        :label="$t('product.store')"
+        item-text="name"
+        item-value="id"
+        required
+      ></v-select>
+    </v-flex>
+    <v-flex xs9 class="pa-2">
+      <v-list two-line>
+        <v-alert
+          :value="true"
+          type="info"
+          v-if="employees.length == 0"
+        >
+          {{ $t('alert.empty') }}
+        </v-alert>
+        <v-list-tile
+          v-for="employee in employees"
+          :key="employee.id"
+          avatar
+          @click="editEmployee(employee.id)"
+        >
+          <v-list-tile-action>
+            <v-icon>star</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ employee.name }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ employee.code }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-avatar v-if="employee.avatar">
+            <img :src="employee.avatar">
+          </v-list-tile-avatar>
+        </v-list-tile>
+      </v-list>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -31,17 +47,39 @@
     name: 'EmployeeList',
     data () {
       return {
-        employees: []
+        storeId: null,
+        stores: [],
+        employees: [],
+        params: new URLSearchParams()
+      }
+    },
+    watch: {
+      storeId (value) {
+        this.params.set('filter[where][storeId]', value);
+        this.getEmployees();
       }
     },
     created () {
-      this.getEmployees();
+      this.getStores();
     },
     methods: {
+      getStores () {
+        let self = this;
+
+        this.$axios.get('/stores')
+        .then(function (response) {
+          self.stores = response.data;
+        })
+        .catch(function (error) {
+          self.stores = [];
+        });
+      },
       getEmployees () {
         let self = this;
 
-        this.$axios.get('/employees')
+        this.$axios.get('/employees', {
+          params: this.params
+        })
         .then(function (response) {
           self.employees = response.data;
         })
