579800e8c54b36e9a9a8ff143af54908952d25ed
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-11 16:35:45 -0600

    close drawer on theme change

--- src/store.js
+++ src/store.js
@@ -24,6 +24,7 @@ const store = new Vuex.Store({
       state.drawer = !state.drawer;
     },
     darkThemeToggle (state) {
+      state.drawer = !state.drawer;
       state.dark = !state.dark;
     }
   },
