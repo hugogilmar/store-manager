0d2ee8c634b023fcccad4412c3727db0c16772d1
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2019-01-19 13:26:30 -0600

    refactor base template for reports

--- src/modules/reports/views/Charge.vue
+++ src/modules/reports/views/Charge.vue
@@ -2,120 +2,120 @@
   <v-card class="ma-4">
     <v-subheader>{{ $t('report.header.charge') }}</v-subheader>
     <v-layout row wrap>
-      <v-flex xs3 class="pa-4">
-        <v-select
-          v-model="storeId"
-          :items="stores"
-          :rules="[v => !!v || 'Item is required']"
-          :label="$t('report.store')"
-          item-text="name"
-          item-value="id"
-          required
-        ></v-select>
-        <v-menu
-          ref="menu"
-          v-model="dateFromDialog"
-          :nudge-right="40"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          min-width="290px"
-        >
-          <v-text-field
-            slot="activator"
-            v-model="dateFrom"
-            :label="$t('report.dateFrom')"
-            readonly
-          ></v-text-field>
-          <v-date-picker
-            v-model="dateFrom"
-            scrollable
-            locale="es-MX"
-            @input="menu = false"
+      <v-flex xs3>
+        <v-container>
+          <v-select
+            v-model="storeId"
+            :items="stores"
+            :rules="[v => !!v || 'Item is required']"
+            :label="$t('report.store')"
+            item-text="name"
+            item-value="id"
+            required
+          ></v-select>
+          <v-menu
+            ref="menu"
+            v-model="dateFromDialog"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
           >
-          </v-date-picker>
-        </v-menu>
-        <v-menu
-          ref="menu"
-          v-model="dateToDialog"
-          :nudge-right="40"
-          lazy
-          transition="scale-transition"
-          offset-y
-          full-width
-          min-width="290px"
-        >
-          <v-text-field
-            slot="activator"
-            v-model="dateTo"
-            :label="$t('report.dateTo')"
-            readonly
-          ></v-text-field>
-          <v-date-picker
-            v-model="dateTo"
-            scrollable
-            locale="es-MX"
-            @input="menu = false"
-          >
-          </v-date-picker>
-        </v-menu>
-      </v-flex>
-      <v-flex xs9 class="pa-4">
-        <div id="report">
-          <v-container grid-list-md>
-            <h3 class="headline mb-4">{{ $t('report.title.charge') }}</h3>
-            <v-layout wrap>
-              <v-flex xs4>
-                <p class="mb-1">{{ storeName }}</p>
-                <p>
-                  <small>{{ $t('order.store') }}</small>
-                </p>
-              </v-flex>
-              <v-flex xs4>
-                <p class="mb-1">{{ dateFrom }}</p>
-                <p>
-                  <small>{{ $t('report.dateFrom') }}</small>
-                </p>
-              </v-flex>
-              <v-flex xs4>
-                <p class="mb-1">{{ dateTo }}</p>
-                <p>
-                  <small>{{ $t('report.dateTo') }}</small>
-                </p>
-              </v-flex>
-            </v-layout>
-            <v-data-table
-              hide-actions
-              :headers="headers"
-              :items="rows"
+            <v-text-field
+              slot="activator"
+              v-model="dateFrom"
+              :label="$t('report.dateFrom')"
+              readonly
+            ></v-text-field>
+            <v-date-picker
+              v-model="dateFrom"
+              scrollable
+              locale="es-MX"
+              @input="menu = false"
             >
-              <template slot="no-data">
-                <v-alert
-                  :value="true"
-                  type="info"
-                >
-                  {{ $t('alert.empty') }}
-                </v-alert>
-              </template>
-              <template slot="items" slot-scope="report">
-                <tr>
-                  <td class="text-xs-center" width="100">{{ report.item.code }}</td>
-                  <td>{{ report.item.name }}</td>
-                  <td class="text-xs-center" width="160">{{ report.item.quantity }}</td>
-                  <td class="text-xs-right" width="160">{{ report.item.total | currency }}</td>
-                </tr>
-              </template>
-              <template slot="footer">
-                <tr>
-                  <td colspan="2" class="text-xs-right">{{ $t('report.total') }}</td>
-                  <td class="text-xs-center">{{ quantity }}</td>
-                  <td class="text-xs-right">{{ total | currency }}</td>
-                </tr>
-              </template>
-            </v-data-table>
-          </v-container>
-        </div>
+            </v-date-picker>
+          </v-menu>
+          <v-menu
+            ref="menu"
+            v-model="dateToDialog"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <v-text-field
+              slot="activator"
+              v-model="dateTo"
+              :label="$t('report.dateTo')"
+              readonly
+            ></v-text-field>
+            <v-date-picker
+              v-model="dateTo"
+              scrollable
+              locale="es-MX"
+              @input="menu = false"
+            >
+            </v-date-picker>
+          </v-menu>
+        </v-container>
+      </v-flex>
+      <v-flex xs9>
+        <v-container id="report">
+          <h3 class="headline mb-4">{{ $t('report.title.charge') }}</h3>
+          <v-layout wrap>
+            <v-flex xs4>
+              <p class="mb-1">{{ storeName }}</p>
+              <p>
+                <small>{{ $t('order.store') }}</small>
+              </p>
+            </v-flex>
+            <v-flex xs4>
+              <p class="mb-1">{{ dateFrom }}</p>
+              <p>
+                <small>{{ $t('report.dateFrom') }}</small>
+              </p>
+            </v-flex>
+            <v-flex xs4>
+              <p class="mb-1">{{ dateTo }}</p>
+              <p>
+                <small>{{ $t('report.dateTo') }}</small>
+              </p>
+            </v-flex>
+          </v-layout>
+          <v-data-table
+            hide-actions
+            :headers="headers"
+            :items="rows"
+          >
+            <template slot="no-data">
+              <v-alert
+                :value="true"
+                type="info"
+              >
+                {{ $t('alert.empty') }}
+              </v-alert>
+            </template>
+            <template slot="items" slot-scope="report">
+              <tr>
+                <td class="text-xs-center" width="100">{{ report.item.code }}</td>
+                <td>{{ report.item.name }}</td>
+                <td class="text-xs-center" width="160">{{ report.item.quantity }}</td>
+                <td class="text-xs-right" width="160">{{ report.item.total | currency }}</td>
+              </tr>
+            </template>
+            <template slot="footer">
+              <tr>
+                <td colspan="2" class="text-xs-right">{{ $t('report.total') }}</td>
+                <td class="text-xs-center">{{ quantity }}</td>
+                <td class="text-xs-right">{{ total | currency }}</td>
+              </tr>
+            </template>
+          </v-data-table>
+        </v-container>
       </v-flex>
     </v-layout>
     <v-fab-transition>
