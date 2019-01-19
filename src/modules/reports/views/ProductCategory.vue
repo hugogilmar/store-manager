b99b72df89cc4b3b0707f141a09804d219cbdff7
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2019-01-19 14:15:55 -0600

    extract product category report component

--- src/modules/reports/views/ProductCategory.vue
+++ src/modules/reports/views/ProductCategory.vue
@@ -86,66 +86,9 @@
               </p>
             </v-flex>
           </v-layout>
-          <v-subheader>{{ $t('report.header.billable') }}</v-subheader>
-          <v-data-table
-            hide-actions
-            :headers="headers"
-            :items="billableRows"
-          >
-            <template slot="no-data">
-              <v-alert
-                :value="true"
-                type="info"
-              >
-                {{ $t('alert.empty') }}
-              </v-alert>
-            </template>
-            <template slot="items" slot-scope="report">
-              <tr>
-                <td class="text-xs-center" width="100">{{ report.item.code }}</td>
-                <td>{{ report.item.name }}</td>
-                <td class="text-xs-center" width="160">{{ report.item.quantity }}</td>
-                <td class="text-xs-right" width="160">{{ report.item.total | currency }}</td>
-              </tr>
-            </template>
-            <template slot="footer">
-              <tr>
-                <td colspan="2" class="text-xs-right">{{ $t('report.total') }}</td>
-                <td class="text-xs-center">{{ billableQuantity }}</td>
-                <td class="text-xs-right">{{ billableTotal | currency }}</td>
-              </tr>
-            </template>
-          </v-data-table>
-          <v-subheader>{{ $t('report.header.nonBillable') }}</v-subheader>
-          <v-data-table
-            hide-actions
-            :headers="headers"
-            :items="nonBillableRows"
-          >
-            <template slot="no-data">
-              <v-alert
-                :value="true"
-                type="info"
-              >
-                {{ $t('alert.empty') }}
-              </v-alert>
-            </template>
-            <template slot="items" slot-scope="report">
-              <tr>
-                <td class="text-xs-center" width="100">{{ report.item.code }}</td>
-                <td>{{ report.item.name }}</td>
-                <td class="text-xs-center" width="160">{{ report.item.quantity }}</td>
-                <td class="text-xs-right" width="160">{{ report.item.total | currency }}</td>
-              </tr>
-            </template>
-            <template slot="footer">
-              <tr>
-                <td colspan="2" class="text-xs-right">{{ $t('report.total') }}</td>
-                <td class="text-xs-center">{{ nonBillableQuantity }}</td>
-                <td class="text-xs-right">{{ nonBillableTotal | currency }}</td>
-              </tr>
-            </template>
-          </v-data-table>
+          <product-category-report
+            :rows.sync="rows"
+          />
         </v-container>
       </v-flex>
     </v-layout>
@@ -168,8 +111,12 @@
 <script>
   import { mapGetters, mapActions } from 'vuex';
   import _ from 'lodash';
+  import ProductCategoryReport from '../components/ProductCategoryReport.vue';
 
   export default {
+    components: {
+      'product-category-report': ProductCategoryReport
+    },
     data () {
       return {
         storeId: null,
@@ -179,32 +126,6 @@
         rows: [],
         dateFromDialog: false,
         dateToDialog: false,
-        headers: [
-          {
-            text: this.$t('report.code'),
-            align: 'center',
-            sortable: false,
-            value: 'code'
-          },
-          {
-            text: this.$t('report.productCategory'),
-            align: 'left',
-            sortable: false,
-            value: 'name'
-          },
-          {
-            text: this.$t('report.quantity'),
-            align: 'center',
-            sortable: false,
-            value: 'quantity'
-          },
-          {
-            text: this.$t('report.total'),
-            align: 'right',
-            sortable: false,
-            value: 'total'
-          }
-        ]
       }
     },
     computed: {
@@ -224,36 +145,6 @@
       },
       valid () {
         return this.storeId && this.dateFrom && this.dateTo;
-      },
-      billableRows () {
-        return this.rows.filter(function (row) {
-          return row.orderBillable && row.orderLineBillable;
-        });
-      },
-      nonBillableRows () {
-        return this.rows.filter(function (row) {
-          return !row.orderBillable || (row.orderBillable && !row.orderLineBillable);
-        });
-      },
-      billableQuantity () {
-        return this.billableRows.reduce(function (sum, row) {
-          return sum += row.quantity;
-        }, 0);
-      },
-      nonBillableQuantity () {
-        return this.nonBillableRows.reduce(function (sum, row) {
-          return sum += row.quantity;
-        }, 0);
-      },
-      billableTotal () {
-        return this.billableRows.reduce(function (sum, row) {
-          return sum += row.total;
-        }, 0);
-      },
-      nonBillableTotal () {
-        return this.nonBillableRows.reduce(function (sum, row) {
-          return sum += row.total;
-        }, 0);
       }
     },
     watch: {
@@ -346,10 +237,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .v-datatable tfoot td {
-    font-size: 13px;
-    font-weight: 600;
-  }
-</style>
